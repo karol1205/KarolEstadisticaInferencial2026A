389d301ff9d4db7d742304e5adfffbf6915ddd3a
karol1205 <24150011@aguascalientes.tecnm.mx>
2026-09-02 10:42:16 -0600

    Update CSV link in cargar_datos.py

--- Unidad l/codigos/cargar_datos.py
+++ Unidad l/codigos/cargar_datos.py
@@ -1,4 +1,4 @@
 import pandas as pd
-link_csv= ""
+link_csv= "https://github.com/karol1205/EstadisiticaInferencial2026A/blob/main/Unidad%20l/problemario/datos_problema_01.csv"
 df=pd.read_csv(link_csv)
 df
